955cc11248489708afbbd1aad295ce33e2454b1a
anoopvelluv <121963285+anoopvelluv@users.noreply.github.com>
2024-01-26 12:26:48 +0000

    Container Instance Deploy Code

---
+++ platform/terraform_aci/main.tf
@@ -0,0 +1,24 @@
+data "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+}
+
+data "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_container_group" "aci" {
+  name  = "docker-container-instance"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location = data.azurerm_container_group.rg.location
+  ip_address_type     = "public"
+  os_type= "Linux"
+
+  container {
+    name   = "pocimagedemo"
+    image  = "${data.azurerm_container_registry.acr.login_server}/aci:${var.build_id}"
+    cpu    = "0.5"
+    memory = "1.5"
+  }
+
+}
